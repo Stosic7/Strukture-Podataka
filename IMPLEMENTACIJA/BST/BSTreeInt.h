563964098b5a7e35bbf4fb43601a237c91e084da
Luka Stošić <158442187+Stosic7@users.noreply.github.com>
2024-10-03 05:01:05 +0200

    Create BSTreeInt.h

---
+++ IMPLEMENTACIJA/BST/BSTreeInt.h
@@ -0,0 +1,57 @@
+#pragma once
+
+#include "BSTNodeInt.h"
+
+class BSTreeInt
+{
+protected:
+	BSTNodeInt* root;
+	long numOfElements;
+
+	void deleteTree(BSTNodeInt* ptr);
+
+	void inorder(const BSTNodeInt* ptr) const;
+	void preorder(const BSTNodeInt* ptr) const;
+	void postorder(const BSTNodeInt* ptr) const;
+
+	// Dodate funkcije
+	int getMoment(BSTNodeInt* root);
+	int getDeepest(BSTNodeInt* node, BSTNodeInt** deepest, int level);
+	int deleteRightLeaves(BSTNodeInt* p);
+	BSTNodeInt* mirror(BSTNodeInt* node);
+	int countSingleLeavesNodes(BSTNodeInt* p);
+	bool jeList(BSTNodeInt* node);
+	void getMaxDifferenceNodeHelper(BSTNodeInt* root, BSTNodeInt** maxNode, int& maxDiff, int& sumParni, int& sumNeparni);
+
+public:
+	BSTreeInt() { root = nullptr; numOfElements = 0; }
+	~BSTreeInt() { deleteTree(root); }
+
+	bool isEmpty() const { return root == nullptr; }
+
+	bool isInTree(int el) const { return search(el) != nullptr; }
+	BSTNodeInt* search(int el) const;
+
+	void insert(int el);
+	void deleteByCopying(int el);
+	void deleteByMerging(int el);
+
+	void preorder() const { preorder(root); }
+	void inorder() const { inorder(root); }
+	void postorder() const { postorder(root); }
+
+	void iterativePreorder() const;
+	void breadthFirstSearch() const;
+
+  int getMoment(BSTNodeInt* root);
+  int getDeepest(BSTNodeInt* node, BSTNodeInt** deepest, int level);
+  int deleteRightLeaves(BSTNodeInt* p);
+  BSTNodeInt* mirror(BSTNodeInt* node);
+  int countSingleLeavesNodes(BSTNodeInt* p);
+  bool jeList(BSTNodeInt* node);
+  void getMaxDifferenceNodeHelper(BSTNodeInt* root, BSTNodeInt** maxNode, int& maxDiff, int& sumParni, int& sumNeparni);
+
+  // ovde dodajete funkciju iz zadatka i implementirajte je u .cpp pa je pozovete u main
+
+
+};
