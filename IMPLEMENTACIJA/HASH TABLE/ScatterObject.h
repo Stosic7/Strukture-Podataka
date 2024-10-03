ba152900ce9705e7e76995590bc535228647549f
Luka Stošić <158442187+Stosic7@users.noreply.github.com>
2024-10-03 04:53:30 +0200

    Update ScatterObject.h

--- IMPLEMENTACIJA/HASH TABLE/ScatterObject.h
+++ IMPLEMENTACIJA/HASH TABLE/ScatterObject.h
@@ -7,7 +7,7 @@ class ScatterObject :
 public:
 	int status; //0-slobodan, 1-obrisan, 2-zauzet
 	ScatterObject();
-	ScatterObject(const char* k, Kosarkas* v);
+	ScatterObject(const char* k);
 	ScatterObject(ScatterObject const& obj);
 	ScatterObject& operator = (ScatterObject const& obj);
 };
